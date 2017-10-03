081f537e4aee0a7e80893af674c70a42881f6266
JithinKS97 <jithunni.ks@gmail.com>
2017-10-03 19:48:38 +0530

    uploading

---
+++ current/Oscillator.js
@@ -0,0 +1,10 @@
+class Oscillator
+{
+
+  display()
+  {
+    fill(255);
+    noStroke();
+    ellipse(w/10, h/2, w/30, w/30);
+  }
+}
---
+++ current/sketch.js
@@ -0,0 +1,15 @@
+let osc;
+let w = 640;
+let h = 360;
+
+function setup()
+{
+  createCanvas(w, h);
+  osc = new Oscillator();
+}
+
+function draw()
+{
+  background(0);
+  osc.display();
+}
