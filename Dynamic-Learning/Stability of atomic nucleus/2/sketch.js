b52ef609efce83cdaf9bb055b9828a875db00407
JithinKS97 <jithunni.ks@gmail.com>
2017-12-23 23:08:14 +0530

    aa

--- Dynamic-Learning/Stability of atomic nucleus/2/sketch.js
+++ Dynamic-Learning/Stability of atomic nucleus/2/sketch.js
@@ -1,17 +1,70 @@
-var img1, img2, d = 40, p, moving = false;
+var img1, img2, d = 40, p = [], overP= false, curMov= -1;
 
 function setup()
 {
     createCanvas(640, 360);
     img1 = loadImage("Images/proton.png");
-    img2 = loadImage("Images/protonBright.png")
-    p = new Proton(width/2, height/2);
+    img2 = loadImage("Images/protonBright.png");
+    img3 = loadImage("Images/pSource.png");
+    img4 = loadImage("Images/pSourceB.png");
 }
 
 function draw()
 {
+    imageMode(CENTER);
     background(0);
-    p.display();
+    protonSource();
+    protonPop();
+    for(var i=0;i<p.length;i++)
+        p[i].display();
+}
+
+function protonPop()
+{
+    for(var i=0;i<p.length;i++)
+    {
+        if(p[i].moving == true && (p[i].pos.x>width) || p[i].pos.x<0 || p[i].pos.y>height || p[i].pos.y<0)
+        {
+            p.splice(i,1);  //When moved out of canvas, the proton is deleted
+            curMov = -1;    
+            break;  
+        }
+    }
+}
+
+function protonSource()
+{
+    var pos = createVector(width*(9/10), height*(1/10));
+
+    if(dist(mouseX,mouseY,pos.x,pos.y)<d/2)
+    {
+        overP = true;
+        image(img4,pos.x, pos.y);
+    }
+    else
+    {   
+        overP = false;
+        image(img3,pos.x,pos.y);
+    }
+}
+
+function mousePressed()
+{
+    if(overP == true)
+        p.push(new Proton(mouseX, mouseY)); // New proton created
+
+    for(var i=0;i<p.length;i++)
+        if(p[i].mouseIsOver())
+        {
+            p[i].moving = true; //To move the proton if mouse is over it and pressed, moving is set to true, curMov set to i to keep track which proton to stop moving when mouse Released
+            curMov = i;
+        }
+}
+
+function mouseReleased()
+{
+    if(curMov>=0)
+        p[curMov].moving = false; // To stop the movement of the proton
 }
 
 class Proton
@@ -23,7 +76,6 @@ class Proton
     }
     display()
     {
-        imageMode(CENTER);
         if(this.mouseIsOver())
             image(img2, this.pos.x, this.pos.y, d ,d);
         else
@@ -31,7 +83,7 @@ class Proton
 
         if(this.moving == true)
         {
-            this.pos.x = mouseX;
+            this.pos.x = mouseX; // To move the proton
             this.pos.y = mouseY;
         }
     }
@@ -43,16 +95,4 @@ class Proton
             return false;
     }
 
-}
-
-function mousePressed()
-{
-    if(p.mouseIsOver())
-        p.moving = true;
-
-}
-
-function mouseReleased()
-{
-    p.moving = false;
 }
